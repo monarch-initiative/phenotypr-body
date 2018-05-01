9bc50014004e0521c71224dc8008da1f40dbfcc1
Heath Harrelson <harrelst@ohsu.edu>
2018-04-30 17:16:17 -0700

    HBCA-2 Search Mixin
    Add a global mixin that injects the search service into component
    options if the search service is present in Vue options.

--- src/main.js
+++ src/main.js
@@ -6,12 +6,22 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 
+import SearchMixin from './mixins/search';
+import SearchService from './services/search-service';
+
 Vue.config.productionTip = false;
+
+// TODO: get the URL from config
+const searchService = new SearchService('/solr/hpo-pl/select');
+
+// Inject the search service into components
+Vue.mixin(SearchMixin);
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  searchService,
   components: { App },
   template: '<App/>'
 });
