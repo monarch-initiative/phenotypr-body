807ecf38d4c3ca9d6566034bf26dd90bef05d3e0
Heath Harrelson <harrelst@ohsu.edu>
2018-05-08 13:24:12 -0700

    HBCA-5 Questions Page Component
    Add a component for the markup and form surrounding the autocomplete
    input.

---
+++ src/components/QuestionsPage.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="questions-page">
+
+    <form name="questionsForm grid-block large-12 row">
+
+      <div class="form-group body-tool">
+
+        <div class="tool-branding"><img src="/static/body.svg" alt="body" class="tool-icon"></div>
+
+        <p class="question-header"><span class="tool-icon-text">Body:</span> SYMPTOMS SEARCH</p>
+
+        <div class="row small-12 medium-8 large-6 columns align-left question-row">
+          <SearchInput @itemSelected="handleSelection"/>
+        </div>
+
+      </div>
+
+      <div class="saved-terms">
+        <span v-for="(selection, index) in selections"
+              :key="selection.id"
+              class="label symptom-tag">{{ selection.exact_synonym[0] }} <strong @click="removeSelection(index)">X</strong></span>
+      </div>
+
+      <div class="button-container ">
+        <input type="button" class="button medium skip-button-q" value="Done adding symptoms" />
+      </div>
+
+    </form>
+
+  </div>
+</template>
+
+<script>
+import SearchInput from './SearchInput';
+
+export default {
+  name: 'QuestionsPage',
+
+  data() {
+    return {
+      selections: []
+    };
+  },
+
+  components: {
+    SearchInput
+  },
+
+  methods: {
+    handleSelection(item) {
+      this.selections.push(item);
+    },
+
+    removeSelection(index) {
+      this.selections.splice(index, 1);
+    }
+  }
+};
+</script>
---
+++ test/unit/specs/components/QuestionsPage.spec.js
@@ -0,0 +1,57 @@
+import { shallow } from '@vue/test-utils';
+import QuestionsPage from '@/components/QuestionsPage';
+import SearchInput from '@/components/SearchInput';
+
+import exampleResponses from '../../example-responses';
+
+describe('QuestionsPage.vue', () => {
+  test('renders a form with a search input', () => {
+    const wrapper = shallow(QuestionsPage);
+    expect(wrapper.find('form').exists()).toBe(true);
+    expect(wrapper.find(SearchInput).exists()).toBe(true);
+  });
+
+  test('renders a tag for each selected term', () => {
+    const wrapper = shallow(QuestionsPage);
+    const sliceIndex = 2;
+
+    expect(wrapper.findAll('.symptom-tag').length).toEqual(0);
+
+    wrapper.setData({
+      // Take items from index 2 to the end
+      selections: exampleResponses.slice(sliceIndex)
+    });
+
+    expect(wrapper.findAll('.symptom-tag').length).toEqual(exampleResponses.length - sliceIndex);
+  });
+
+  test('selecting an item adds a tag', () => {
+    const wrapper = shallow(QuestionsPage);
+    const expectedItem = exampleResponses[2];
+
+    expect(wrapper.findAll('.symptom-tag').length).toEqual(0);
+
+    wrapper.vm.handleSelection(expectedItem);
+
+    const tags = wrapper.findAll('.symptom-tag');
+    expect(tags.length).toEqual(1);
+    expect(tags.at(0).text()).toContain(expectedItem.exact_synonym[0]);
+  });
+
+  test('clicking the X on a tag removes it', () => {
+    const expectedItem = exampleResponses[3];
+    const wrapper = shallow(QuestionsPage, {
+      data: {
+        selections: [expectedItem]
+      }
+    });
+
+    let tags = wrapper.findAll('.symptom-tag');
+    expect(tags.length).toEqual(1);
+
+    tags.at(0).find('strong').trigger('click');
+
+    tags = wrapper.findAll('.symptom-tag');
+    expect(tags.length).toEqual(0);
+  });
+});
