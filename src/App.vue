6099bc5050e4895cee6b12c823cdab1621d0fc2c
Heath Harrelson <harrelst@ohsu.edu>
2018-05-08 12:18:57 -0700

    HBCA-5 Integrate Header and Grid
    Integrate the header and grid from the mockup into `App.vue`.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <template>
-  <div id="app">
-    <div class="grid-container">
-      <router-view/>
+  <div id="app" class="wrapper">
+    <div>
+      <div>
+        <div class="top-bar header">
+         </div>
+        <nav class="second-bar nav-bar-wrapper">
+          <span class="nav-bar-text">
+            <a href="https://phenotypr.com/index.html" class="nav-links-home"><strong>PHENOTYPR</strong></a>
+            <a href="/#/" class="nav-links-home">Body Tool</a>
+          </span>
+        </nav>
+      </div>
+    </div>
+
+    <div class="grid-content">
+      <div class="grid-container">
+        <router-view/>
+      </div>
     </div>
   </div>
 </template>
