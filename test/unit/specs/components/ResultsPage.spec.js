7f2ce6330dedf95cfc9bf92fd968ca67b8216adb
Heath Harrelson <harrelst@ohsu.edu>
2018-05-25 16:26:33 -0700

    HBCA-4 Download PDF
    Generate and download a PDF constructed from the HPO terms when
    the button on the `ResultsPage` component is clicked.

--- test/unit/specs/components/ResultsPage.spec.js
+++ test/unit/specs/components/ResultsPage.spec.js
@@ -2,10 +2,16 @@ import Vuex from 'vuex';
 import { shallow, createLocalVue } from '@vue/test-utils';
 
 import ResultsPage from '@/components/ResultsPage';
+import PdfRenderer from '@/utils/pdf-renderer';
+import isExportDocument from '@/utils/is-export-document';
 import exampleResponses from '../../example-responses';
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
+
+jest.mock('@/utils/pdf-renderer', () => {
+  return jest.fn().mockImplementation(() => ({}));
+});
 
 describe('ResultsPage.vue', () => {
   let store;
@@ -21,5 +27,25 @@ describe('ResultsPage.vue', () => {
   test('renders the selected terms', () => {
     const wrapper = shallow(ResultsPage, { store, localVue });
     expect(wrapper.findAll('tbody tr').length).toEqual(exampleResponses.length);
+  });
+
+  test('generates and downloads PDF when the button is clicked', () => {
+    const mockRender = jest.fn();
+    const mockPdf = { download: jest.fn() };
+
+    mockRender.mockReturnValue(mockPdf);
+    PdfRenderer.mockImplementation(() => {
+      return {
+        renderDocument: mockRender
+      };
+    });
+
+    const wrapper = shallow(ResultsPage, { store, localVue });
+    wrapper.find('input.button').trigger('click');
+
+    expect(PdfRenderer).toHaveBeenCalled();
+    expect(mockRender).toHaveBeenCalled();
+    expect(isExportDocument(mockRender.mock.calls[0][0])).toBe(true);
+    expect(mockPdf.download).toHaveBeenCalled();
   });
 });
