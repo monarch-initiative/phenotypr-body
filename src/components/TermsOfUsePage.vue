f40b49ad9aa8e1210c3b696931f915eeafd17a67
Weldon Sams <sams@ohsu.edu>
2018-06-06 12:18:22 -0700

    HBCA-22 terms of use route
    Add terms of use route and redirect to it for all other pages if required.

---
+++ src/components/TermsOfUsePage.vue
@@ -0,0 +1,11 @@
+<template>
+<div>
+  <p>Terms of Use</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'TermsOfUsePage'
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -3,22 +3,42 @@ import Router from 'vue-router';
 
 import SearchPage from '@/components/SearchPage';
 import ResultsPage from '@/components/ResultsPage';
+import TermsOfUsePage from '@/components/TermsOfUsePage';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
-      redirect: '/search'
+      redirect: '/search',
+      meta: { requireTermsOfUse: true }
     },
     {
       path: '/search',
-      component: SearchPage
+      component: SearchPage,
+      meta: { requireTermsOfUse: true }
     },
     {
       path: '/results',
-      component: ResultsPage
+      component: ResultsPage,
+      meta: { requireTermsOfUse: true }
+    },
+    {
+      path: '/terms-of-use',
+      component: TermsOfUsePage
     }
   ]
 });
+
+router.beforeResolve((to, from, next) => {
+  if (to.meta.requireTermsOfUse) {
+    next({
+      path: '/terms-of-use'
+    });
+  } else {
+    next();
+  }
+});
+
+export default router;
