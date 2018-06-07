431a89d4bc82c71abc41a04a7adfe31f5e61ddf3
Heath Harrelson <harrelst@ohsu.edu>
2018-06-07 09:24:18 -0700

    HBCA-22 Router Tests
    Add tests for route configuration and guard function.

--- src/router/index.js
+++ src/router/index.js
@@ -1,47 +1,51 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
+import store from '@/store';
 import SearchPage from '@/components/SearchPage';
 import ResultsPage from '@/components/ResultsPage';
 import TermsOfUsePage from '@/components/TermsOfUsePage';
-import store from '@/store/index.js';
 
 Vue.use(Router);
 
 const termsOfUsePath = '/terms-of-use';
 
-const router = new Router({
-  routes: [
-    {
-      path: '/',
-      redirect: '/search',
-      meta: { requireTermsOfUse: true }
-    },
-    {
-      path: '/search',
-      component: SearchPage,
-      meta: { requireTermsOfUse: true }
-    },
-    {
-      path: '/results',
-      component: ResultsPage,
-      meta: { requireTermsOfUse: true }
-    },
-    {
-      path: termsOfUsePath,
-      component: TermsOfUsePage
-    }
-  ]
-});
-
-router.beforeResolve((to, from, next) => {
-  if (to.meta.requireTermsOfUse && !store.state.termsOfUseAccepted) {
+export const checkTermsAccepted = (to, from, next) => {
+  if (to.meta && to.meta.requireTermsOfUse && !store.state.termsOfUseAccepted) {
     next({
       path: termsOfUsePath
     });
   } else {
     next();
   }
+};
+
+export const routes = [
+  {
+    path: '/',
+    redirect: '/search',
+    meta: { requireTermsOfUse: true }
+  },
+  {
+    path: '/search',
+    component: SearchPage,
+    meta: { requireTermsOfUse: true }
+  },
+  {
+    path: '/results',
+    component: ResultsPage,
+    meta: { requireTermsOfUse: true }
+  },
+  {
+    path: termsOfUsePath,
+    component: TermsOfUsePage
+  }
+];
+
+const router = new Router({
+  routes
 });
+
+router.beforeResolve(checkTermsAccepted);
 
 export default router;
