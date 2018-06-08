86dd5200bbe01b73b6217a286b2d32319afbb2f9
Heath Harrelson <harrelst@ohsu.edu>
2018-06-08 11:49:53 -0700

    Calculate Score When Terms are Selected
    Dispatch the `calculateQualityScore` action when terms are selected
    or deleted.

--- test/unit/specs/components/SearchPage.spec.js
+++ test/unit/specs/components/SearchPage.spec.js
@@ -9,7 +9,7 @@ const localVue = createLocalVue();
 localVue.use(Vuex);
 
 describe('SearchPage.vue', () => {
-  let store, state, mutations;
+  let store, state, mutations, actions;
 
   beforeEach(() => {
     mutations = {
@@ -17,11 +17,15 @@ describe('SearchPage.vue', () => {
       removeTermAtIndex: jest.fn()
     };
 
+    actions = {
+      calculateQualityScore: jest.fn()
+    };
+
     state = {
       selectedTerms: exampleTerms.slice(2)
     };
 
-    store = new Vuex.Store({ state, mutations });
+    store = new Vuex.Store({ state, mutations, actions });
   });
 
   test('renders a form with a search input', () => {
@@ -36,7 +40,7 @@ describe('SearchPage.vue', () => {
     expect(wrapper.findAll('.symptom-tag').length).toEqual(tagCount);
   });
 
-  test('selecting an item commits an addTerm mutation', () => {
+  test('selecting an item adds the term to the store', () => {
     const wrapper = shallow(SearchPage, { store, localVue });
     const expectedItem = exampleTerms[2];
 
@@ -44,12 +48,28 @@ describe('SearchPage.vue', () => {
     expect(mutations.addTerm).toHaveBeenCalledWith(state, expectedItem);
   });
 
-  test('clicking the X on a tag commits a removeTermAtIndex mutation', () => {
+  test('selecting an item recalculates the quality score', () => {
+    const wrapper = shallow(SearchPage, { store, localVue });
+    const expectedItem = exampleTerms[2];
+
+    wrapper.vm.handleSelection(expectedItem);
+    expect(actions.calculateQualityScore).toHaveBeenCalled();
+  });
+
+  test('clicking the X on a tag removes it from the store', () => {
     const wrapper = shallow(SearchPage, { store, localVue });
     const tagRemovalButtons = wrapper.findAll('.symptom-tag strong');
 
     tagRemovalButtons.at(1).trigger('click');
     expect(mutations.removeTermAtIndex).toHaveBeenCalledWith(state, 1);
+  });
+
+  test('clicking the X on a tag recalculates the quality score', () => {
+    const wrapper = shallow(SearchPage, { store, localVue });
+    const tagRemovalButtons = wrapper.findAll('.symptom-tag strong');
+
+    tagRemovalButtons.at(1).trigger('click');
+    expect(actions.calculateQualityScore).toHaveBeenCalled();
   });
 
   test('the button is disabled when no terms are selected', () => {
