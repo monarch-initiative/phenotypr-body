a235d458266c2c111515e8f9a93315349ddc2e7a
Heath Harrelson <harrelst@ohsu.edu>
2018-06-11 09:49:40 -0700

    Terms of Use Text
    Update terms of use to use text provided by Kara.

--- src/components/TermsOfUsePage.vue
+++ src/components/TermsOfUsePage.vue
@@ -1,7 +1,10 @@
 <template>
 <div class="grid-container body-search-content">
   <h2>Terms of Use</h2>
-  <p>Do you accept?</p>
+  <p>This tool does not provide medical advice. It is intended for informational and
+    educational purposes only. By submitting information through the Phenotpyr platform you
+    understand and accept the <a href="https://phenotypr.com/terms.html" title="Phenotypr terms of use">terms of use</a>
+    to this site.</p>
   <form @submit.prevent>
     <a ref="terms-no-button" href="https://phenotypr.com/index.html" class="button rounded alert">No</a>
     <input type="button" value="Yes" class="button rounded" @click="acceptTermsOfUse">
