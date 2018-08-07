9c5d860a25d806b41e1d9a8c3f36d8d37e546998
Erik Benton <benton@ohsu.edu>
2018-08-07 09:28:51 -0700

    Refactor Systems store

--- src/store/systems.js
+++ src/store/systems.js
@@ -1,22 +1,21 @@
-export default{
+const systems = [
+  {id: 'HP:0000708', label: 'Behavioral / psychiatric'},
+  {id: 'HP:0001871', label: 'Blood / bleeding'},
+  {id: 'HP:0000924', label: 'Bones / cartilage / connective tissue'},
+  {id: 'HP:0000707', label: 'Brain / nervous system'},
+  {id: 'HP:0002664', label: 'Cancer'},
+  {id: 'HP:0025031', label: 'Digestive / gastrointestinal (GI)'},
+  {id: 'HP:0000598', label: 'Ears / hearing'},
+  {id: 'HP:0000478', label: 'Eyes / vision'},
+  {id: 'HP:0001507', label: 'Growth'},
+  {id: 'HP:0000152', label: 'Head/Face/Neck'},
+  {id: 'HP:0001626', label: 'Heart / blood vessels'},
+  {id: 'HP:0000818', label: 'Hormone / Endocrine'},
+  {id: 'HP:0002715', label: 'Immune system'},
+  {id: 'HP:0000119', label: 'Kidneys / bladder / genitals'},
+  {id: 'HP:0002086', label: 'Lungs / breathing'},
+  {id: 'HP:0003011', label: 'Muscles'},
+  {id: 'HP:0001574', label: 'Skin'}
+];
 
-  systems: [
-    {id: 'HP:0000708', label: 'Behavioral / psychiatric'},
-    {id: 'HP:0001871', label: 'Blood / bleeding'},
-    {id: 'HP:0000924', label: 'Bones / cartilage / connective tissue'},
-    {id: 'HP:0000707', label: 'Brain / nervous system'},
-    {id: 'HP:0002664', label: 'Cancer'},
-    {id: 'HP:0025031', label: 'Digestive / gastrointestinal (GI)'},
-    {id: 'HP:0000598', label: 'Ears / hearing'},
-    {id: 'HP:0000478', label: 'Eyes / vision'},
-    {id: 'HP:0001507', label: 'Growth'},
-    {id: 'HP:0000152', label: 'Head/Face/Neck'},
-    {id: 'HP:0001626', label: 'Heart / blood vessels'},
-    {id: 'HP:0000818', label: 'Hormone / Endocrine'},
-    {id: 'HP:0002715', label: 'Immune system'},
-    {id: 'HP:0000119', label: 'Kidneys / bladder / genitals'},
-    {id: 'HP:0002086', label: 'Lungs / breathing'},
-    {id: 'HP:0003011', label: 'Muscles'},
-    {id: 'HP:0001574', label: 'Skin'}
-  ]
-};
+export default systems;
