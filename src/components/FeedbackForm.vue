c13e4f901fb0878ff6d21ee377921541c28ae156
Heath Harrelson <harrelst@ohsu.edu>
2018-08-07 17:09:01 -0700

    Feedback Form Component
    Implement a basic component for getting feedback on whether the
    participant found all their symptoms.

--- src/components/FeedbackForm.vue
+++ src/components/FeedbackForm.vue
@@ -1,9 +1,89 @@
 <template>
-  <div></div>
+  <div class="grid-container body-search-content">
+    <!-- heading -->
+    <div class="grid-x grid-margin-x">
+      <div class="cell">
+        <div class="media-object">
+          <div class="media-object-section">
+            <div>
+              <img src="../../static/body.svg">
+            </div>
+          </div>
+
+          <div class="media-object-section">
+            <h1><strong>Body:</strong> Application Feedback</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="grid-x grid-margin-x">
+      <div class="cell medium-8 large-6">
+        Did you find all the symptoms you were searching for?
+
+        <label>
+          Yes
+          <input type="radio" name="foundAllConditions" value="1" :checked="trueChecked" @click="setFeedback">
+        </label>
+        <label>
+          No
+          <input type="radio" name="foundAllConditions" value="0" :checked="falseChecked" @click="setFeedback">
+        </label>
+      </div>
+    </div>
+
+    <!-- buttons -->
+    <div class="grid-x grid-margin-x button-container">
+      <div class="cell large-4 text-left">
+        <input type="button" name="backButton" value="Go Back" class="button rounded" @click="goBack">
+      </div>
+      <div class="cell large-4 text-right">
+        <input type="button" name="submitButton" value="Done" class="button rounded" :disabled="feedbackIncomplete" @click="goToResults">
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 export default {
-  name: 'FeedbackForm'
+  name: 'FeedbackForm',
+
+  methods: {
+    setFeedback(evt) {
+      const value = evt.target.value === '1';
+      this.$store.commit('setFoundAllConditions', value);
+    },
+
+    goBack() {
+      this.$router.push('/search');
+    },
+
+    goToResults() {
+      this.$router.push('/results');
+    }
+  },
+
+  computed: {
+    ...mapState({
+      foundAllConditions: 'foundAllConditions'
+    }),
+
+    feedbackIncomplete() {
+      const { foundAllConditions } = this;
+      return foundAllConditions === null;
+    },
+
+    trueChecked() {
+      const { foundAllConditions } = this;
+      return foundAllConditions === true;
+    },
+
+    falseChecked() {
+      const { foundAllConditions } = this;
+      return foundAllConditions === false;
+    }
+  }
 };
 </script>
---
+++ test/unit/specs/components/FeedbackForm.spec.js
@@ -0,0 +1,150 @@
+import Vuex from 'vuex';
+import { shallow, createLocalVue } from '@vue/test-utils';
+
+import FeedbackForm from '@/components/FeedbackForm';
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+describe('FeedbackForm.vue', () => {
+  let store, state, mutations;
+
+  beforeEach(() => {
+    mutations = {
+      setFoundAllConditions: jest.fn()
+    };
+
+    state = {
+      foundAllConditions: null
+    };
+
+    store = new Vuex.Store({ state, mutations });
+  });
+
+  test('renders a pair of radio buttons', () => {
+    const wrapper = shallow(FeedbackForm, { store, localVue });
+    expect(wrapper.findAll('input[type=radio]').length).toEqual(2);
+  });
+
+  test('checked radio button matches state', () => {
+    const checkedRadioSelector = 'input[type=radio]:checked';
+    const wrapper = shallow(FeedbackForm, { store, localVue });
+
+    // nothing selected initially
+    let checkedRadios = wrapper.findAll(checkedRadioSelector);
+    expect(checkedRadios.length).toEqual(0);
+
+    state.foundAllConditions = false;
+    checkedRadios = wrapper.findAll(checkedRadioSelector);
+    expect(checkedRadios.length).toEqual(1);
+    expect(wrapper.find('input[value="0"]').is(':checked')).toBeTruthy();
+
+    state.foundAllConditions = true;
+    checkedRadios = wrapper.findAll(checkedRadioSelector);
+    expect(checkedRadios.length).toEqual(1);
+    expect(wrapper.find('input[value="1"]').is(':checked')).toBeTruthy();
+  });
+
+  test('clicking radio buttons mutates the state', () => {
+    const wrapper = shallow(FeedbackForm, { store, localVue });
+    const checkBoxes = wrapper.findAll('input[type=radio]');
+
+    checkBoxes.at(0).trigger('click');
+    expect(mutations.setFoundAllConditions).toHaveBeenCalledWith(state, true);
+
+    mutations.setFoundAllConditions.mockReset();
+    checkBoxes.at(1).trigger('click');
+    expect(mutations.setFoundAllConditions).toHaveBeenCalledWith(state, false);
+  });
+
+  test('submit button is disabled until feedback is complete', () => {
+    const wrapper = shallow(FeedbackForm, { store, localVue });
+    const submitButton = wrapper.find('input[name=submitButton]');
+
+    // should be disabled if nothing has been chosen
+    expect(submitButton.attributes().disabled).toBeTruthy();
+
+    // should be enabled if feedback has been provided
+    state.foundAllConditions = true;
+    expect(submitButton.attributes().disabled).toBeFalsy();
+
+    state.foundAllConditions = false;
+    expect(submitButton.attributes().disabled).toBeFalsy();
+  });
+
+  test('clicking the back button transitions to the search route', () => {
+    const mocks = {
+      $router: {
+        push: jest.fn()
+      }
+    };
+
+    const wrapper = shallow(FeedbackForm, { store, localVue, mocks });
+
+    wrapper.find('input[name=backButton]').trigger('click');
+    expect(mocks.$router.push).toHaveBeenCalledWith('/search');
+  });
+
+  test('clicking the submit button transitions to the results route', () => {
+    const mocks = {
+      $router: {
+        push: jest.fn()
+      }
+    };
+
+    state.foundAllConditions = true;
+    const wrapper = shallow(FeedbackForm, { store, localVue, mocks });
+
+    wrapper.find('input[name=submitButton]').trigger('click');
+    expect(mocks.$router.push).toHaveBeenCalledWith('/results');
+  });
+
+  describe('computed properties', () => {
+    test('state mapping', () => {
+      const wrapper = shallow(FeedbackForm, { store, localVue });
+      expect(wrapper.vm.foundAllConditions).toBeNull();
+
+      state.foundAllConditions = true;
+      expect(wrapper.vm.foundAllConditions).toBe(true);
+    });
+
+    test('feedbackIncomplete', () => {
+      const wrapper = shallow(FeedbackForm, { store, localVue });
+
+      // is true if foundAllConditions is null
+      expect(wrapper.vm.feedbackIncomplete).toBe(true);
+
+      // is false if foundAllConditions is not null
+      state.foundAllConditions = true;
+      expect(wrapper.vm.feedbackIncomplete).toBe(false);
+      state.foundAllConditions = false;
+      expect(wrapper.vm.feedbackIncomplete).toBe(false);
+    });
+
+    test('trueChecked', () => {
+      const wrapper = shallow(FeedbackForm, { store, localVue });
+
+      // false when foundAllConditions is null or false
+      expect(wrapper.vm.trueChecked).toBe(false);
+      state.foundAllConditions = false;
+      expect(wrapper.vm.trueChecked).toBe(false);
+
+      // true when foundAllConditions is true
+      state.foundAllConditions = true;
+      expect(wrapper.vm.trueChecked).toBe(true);
+    });
+
+    test('falseChecked', () => {
+      const wrapper = shallow(FeedbackForm, { store, localVue });
+
+      // false when foundAllConditions is null or true
+      expect(wrapper.vm.falseChecked).toBe(false);
+      state.foundAllConditions = true;
+      expect(wrapper.vm.falseChecked).toBe(false);
+
+      // true when foundAllConditions is false
+      state.foundAllConditions = false;
+      expect(wrapper.vm.falseChecked).toBe(true);
+    });
+  });
+});
