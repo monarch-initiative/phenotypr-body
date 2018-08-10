810c1c4156183fd5441f3a88c55e4d02a585b4aa
Heath Harrelson <harrelst@ohsu.edu>
2018-08-10 14:07:34 -0700

    Term Conversion Utility
    Add a utility that converts terms returned from the search service
    to the format expected by persistence.

--- test/unit/example-session-data.js
+++ test/unit/example-session-data.js
@@ -1,12 +1,11 @@
 import uuid from 'uuid/v4';
 import bodySystems from '@/store/systems';
 import exampleTerms from './example-terms';
-
-const mapTermShape = ({ id, label, symptomText }) => ({ id, label, symptom: symptomText });
+import { convertTerm } from '@/utils/persistence-utils';
 
 export default {
   session_id: uuid(),
   selected_systems: bodySystems.slice(0, 1).map(system => system.id),
-  selected_terms: exampleTerms.slice(0, 1).map(mapTermShape),
+  selected_terms: exampleTerms.slice(0, 1).map(convertTerm),
   found_all: true
 };
